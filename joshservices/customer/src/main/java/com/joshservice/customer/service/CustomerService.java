42777e4df1097e92eede35419afe46c6d47a3703
Jose Alejandro <jscuesta96@gmail.com>
2022-04-12 23:41:17 +0200

    notification module

--- joshservices/customer/src/main/java/com/joshservice/customer/service/CustomerService.java
+++ joshservices/customer/src/main/java/com/joshservice/customer/service/CustomerService.java
@@ -2,13 +2,14 @@ package com.joshservice.customer.service;
 
 import com.joshservice.clients.fraud.FraudCheckResponse;
 import com.joshservice.clients.fraud.FraudClient;
+import com.joshservice.clients.notification.NotificationClient;
+import com.joshservice.clients.notification.NotificationRequest;
 import com.joshservice.customer.entities.Customer;
 import com.joshservice.customer.models.CustomerRegistrationRequest;
 import com.joshservice.customer.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
@@ -16,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerService{
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private final NotificationClient notificationClient;
     private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
@@ -37,6 +38,15 @@ public class CustomerService{
         if(fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("fraudster");
         }
-        // todo: send notification
+        // send notification
+        // todo: make it async. i.e add to queue
+        notificationClient.sendNotification(
+                new NotificationRequest(
+                        customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi %s, welcome to the system...",
+                                customer.getFirstName())
+                )
+        );
     }
 }
